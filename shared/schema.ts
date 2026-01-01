4d658a97d4c45d2cc98fdc95f1ab6f38acf5277a
maxmicko <mickomaksa123@gmail.com>
2026-01-01 20:23:29 +0100

    lmao

--- shared/schema.ts
+++ shared/schema.ts
@@ -8,7 +8,7 @@ export const users = pgTable("users", {
   username: text("username").notNull().unique(),
   password: text("password").notNull(),
 });
-
+//lmao
 export const insertUserSchema = createInsertSchema(users).pick({
   username: true,
   password: true,
